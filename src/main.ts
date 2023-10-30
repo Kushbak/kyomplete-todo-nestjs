152b8bf9401de9d5862f60489ae0228715057b86
Kushbak Mamytov <mamytovkushbak@gmail.com>
2023-10-30 17:35:48 +0600

    add env vars to cors and port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3333);
+  const app = await NestFactory.create(AppModule, { cors: false });
+  const corsOptions: CorsOptions = {
+    origin: process.env.ORIGIN || 'http://localhost:3000', // or your frontend application's URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
