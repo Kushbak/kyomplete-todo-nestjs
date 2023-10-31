8d0ad7b22514a37c7f32bd996b3769b49ddef732
Kushbak Mamytov <mamytovkushbak@gmail.com>
2023-11-01 03:47:05 +0600

    change config keys to vercel postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,10 +14,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
+        port: +configService.get<number>('DB_PORT') || 5432,
+        username: configService.get('DB_USER'),
         password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        database: configService.get('DB_DATABASE'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
       }),
